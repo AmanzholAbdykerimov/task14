54df806fd3db51e0eca6359d34da79065d268986
Aman <109012103+AmanzholAbdykerimov@users.noreply.github.com>
2022-08-18 19:12:48 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Class cl=new Class(12,"Klass", 4, 9, 6);
-        cl.Class1();
+        System.out.println(cl.toString());
     }
 }
